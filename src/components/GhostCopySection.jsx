bddeff17a446c77e96a99b867437a3b12e1e2857
Christopher Jackson <chrstphrgvn@Claudias-MacBook-Pro.local>
2025-07-21 01:51:21 -0700

    spacing and telegram icon

--- src/components/GhostCopySection.jsx
+++ src/components/GhostCopySection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ghost1 from "../assets/ghost-1.svg";
 import booGIF from "../assets/boo-gif.gif";
 import ghostCopy from "../assets/ghost-copy-only.svg"
+import telegramPlane from "../assets/telegram.png"
 
 import "../styles/GhostCopySection.css";
 
@@ -11,6 +12,12 @@ const GhostCopySection = () => {
       <img src={ghost1} alt="Floating Ghost" className="floating-ghost" />
       <img src={booGIF} alt= "Boo GIF" className="booGIF" />
       <img src={ghostCopy} />
+      <a
+        href="https://t.me/nippletips"
+        target="_blank"
+        rel="noopener noreferrer">
+      <img src={telegramPlane} className="telePlane" />
+      </a>
     </div>
   );
 };
