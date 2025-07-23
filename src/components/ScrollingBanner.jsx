a3420c98dbaadbac711bdce571dec1d972c2ed1d
Christopher Jackson <elppinspit@gmail.com>
2025-07-23 00:30:36 -0700

    updated banner

--- src/components/ScrollingBanner.jsx
+++ src/components/ScrollingBanner.jsx
@@ -3,7 +3,7 @@ import "./ScrollingBanner.css";
 
 const ScrollingBanner = () => {
   const message =
-    ">>> JOIN THE NPT PRESALE <<<>>> ( • )( • ) <<<>>> ROUND 1 OF 6 <<<>>> ( • )( • ) <<<>>> $0.001 PER TOKEN <<<>>> ( • )( • ) <<<>>> PRICE INCREASES BY $0.0002 EACH ROUND <<<>>> ( • )( • ) <<<>>> BUY WITH USDC OR POL <<<>>> ( • )( • ) <<<>>> STAKE FOR REWARDS <<<>>> ( • )( • ) <<<>>> LET'S GIVE BOOBS THE SUPPORT THEY DESERVE <<<>>> ( • )( • ) <<<>>> LAST DAY TO STAKE OCT. 01, 2025 <<<>>> ( • )( • ) <<<>>> REWARDS DROP ON 0CT. 31, 2025 <<<>>> ( • )( • ) <<<";
+    ">>> JOIN THE NPT PRESALE <<<>>> ( • )( • ) <<<>>> ROUND 1 OF 6 <<<>>> ( • )( • ) <<<>>> $0.001 PER TOKEN <<<>>> ( • )( • ) <<<>>> PRICE INCREASES BY $0.0002 EACH ROUND <<<>>> ( • )( • ) <<<>>> BUY WITH USDC OR POL <<<>>> ( • )( • ) <<<>>> STAKE FOR REWARDS <<<>>> ( • )( • ) <<<>>> LAST DAY TO STAKE OCT. 01, 2025 <<<>>> ( • )( • ) <<<>>> REWARDS DROP ON 0CT. 31, 2025 <<<>>> ( • )( • ) <<<>>> LET'S GIVE BOOBS THE SUPPORT THEY DESERVE <<<>>> ( • )( • ) <<<";
 
   return (
     <div className="sticky-banner">
